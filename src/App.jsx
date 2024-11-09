aadbece3a0c2d1aec2019f3a29cb4b781c2d9f96
Rifannvl <riffanafendi@gmail.com>
2024-11-09 13:56:51 +0700

    menambahkan base layout untuk semua page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import BaseLayout from "./layout/BaseLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <BaseLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BaseLayout>
     </BrowserRouter>
   );
 }
